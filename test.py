3ffc5da50038829ebcf93a470f53caa89bfd7210
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-07-16 15:44:57 -1000

    Remove comments and excessive import statements.

--- test.py
+++ test.py
@@ -1,33 +1,8 @@
-"""
-This file is the C in MVC. This is the middleman between the front and
-back ends.  Information comes here to decide where to go, next.
-"""
+# This file is the C in MVC. This is the middleman between the front and
+# back ends.  Information comes here to decide where to go, next.
 
-from Antenna_Snapshot import Antenna_Snapshot
-from Datum import Datum
-from attributes import attributes
-from bson.objectid import ObjectId # For ObjectId to work
-from chart_picker import *
 from flask import Flask, render_template,request,redirect,url_for # For flask implementation
-# from flask_bootstrap import Bootstrap
-# from flask_wtf import FlaskForm
-# from flask_wtf.file import FileField
-# from pymongo import MongoClient # Database connector
-# from wtforms import TextField, HiddenField, ValidationError, RadioField,\
-    # BooleanField, SubmitField, IntegerField, FormField, validators
-# from wtforms.validators import Required
-
-import time
-from datetime import datetime
-from mongoengine import *
-import plotly
-# import pymongo
-import plotly.figure_factory as ff
-from plotly.graph_objs import Layout, Scatter
-# import search_route
 from search_route import *
-
-
 
 app = Flask(__name__)
 # Bootstrap(app)
