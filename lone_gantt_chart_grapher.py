48a9fc3143aa402d082b1aec5a9c379098dd76fe
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-07-16 15:53:04 -1000

    Add functioning single-bar gantt chart plotter.

---
+++ lone_gantt_chart_grapher.py
@@ -0,0 +1,31 @@
+# This file defines the
+
+from search_imports import *
+
+def lone_gantt_chart_grapher(graph, title):
+
+    colors = {}
+    color_count = 0
+    for color in graph.color_labels:
+        colors[color] = rgb_colors[color_count]
+        color_count += 1
+
+    graph.data_gettin_visualized = ff.create_gantt(
+        graph.attr_states,
+        colors=colors,
+        index_col='Resource',
+        show_colorbar=True,
+        group_tasks=True
+    )
+
+    plotly.offline.plot(
+        graph.data_gettin_visualized,
+        filename=r"visualization.html",
+        auto_open=True
+    )
+# finally:
+    # TODO: zero plotly data corner case
+    # try to delete the old graph, somehow
+# finally:
+    # del graph
+# print(f"len(graph.y_values_per_antenna[0]): { len( graph.y_values_per_antenna[0] ) }")
