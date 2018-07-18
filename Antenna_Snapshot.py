d9e4221b085ec51307ee4adcd9f91aa4ea8c0804
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-07-17 14:50:06 -1000

    Change default string value from DATA to dummy data.

--- Antenna_Snapshot.py
+++ Antenna_Snapshot.py
@@ -1,7 +1,5 @@
-"""
-This file defines the Antenna_Snapshot class, which is a container for all the
-available telescope data associated with a single antenna at a single time.
-"""
+# This file defines the Antenna_Snapshot class, which is a container for all the
+# available telescope data associated with a single antenna at a single time.
 
 from mongoengine import *
 
@@ -10,13 +8,13 @@ class Antenna_Snapshot(EmbeddedDocument):
     # http://docs.mongoengine.org/apireference.html#fields
     sel1X = IntField(default=0) # also in the logCorr_X file
     sel2X = IntField(default=0) # also in the logCorr_X file
-    intswX = StringField(default="DATA") # also in the logCorr_X file
-    hybrid_selX = StringField(default="DATA") # also in the logCorr_X file
+    intswX = StringField(default="dummy data") # also in the logCorr_X file
+    hybrid_selX = StringField(default="dummy data") # also in the logCorr_X file
     intLenX = FloatField(default=0.0) # also in the logCorr_X file
     sel1Y = IntField(default=0) # also in the logCorr_Y file
     sel2Y = IntField(default=0) # also in the logCorr_Y file
-    intswY = StringField(default="DATA") # also in the logCorr_Y file
-    hybrid_selY = StringField(default="DATA") # also in logCorr_Y
+    intswY = StringField(default="dummy data") # also in the logCorr_Y file
+    hybrid_selY = StringField(default="dummy data") # also in logCorr_Y
     intLenY = FloatField(default=0.0) # also in the logCorr_Y file
     iflo_x = FloatField(default=0.0) # also in the logIFLO_X file
     iflo_y = FloatField(default=0.0) # also in the logIFLO_Y file
