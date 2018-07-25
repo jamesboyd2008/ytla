a0143db1f754ddd6cd6480c189779bbfc8081ee9
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-07-25 09:29:16 -1000

    Remove obsolete tests.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -7,20 +7,6 @@ class TestHelpers(unittest.TestCase):
     import sys
     sys.path.append('.')
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
     def test_timerator(self):
         from timerator import timerator
         self.assertEqual(timerator("01/23/2019 12:27 PM"),"2019-01-23 12:27:00")
