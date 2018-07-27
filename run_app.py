338b1f16ddbec93faab56d7300bafcd29b325cf2
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-07-26 16:34:08 -1000

    Add useful comment.

--- run_app.py
+++ run_app.py
@@ -1,7 +1,4 @@
-# under normal circumstances, this script would not be necessary. the
-# client would have its own setup.py and be properly installed;
-# however since it is not bundled in the sdist package, we need some hacks
-# to make it work
+# This script creates and runs an instance of the application.
 
 import os
 import sys
