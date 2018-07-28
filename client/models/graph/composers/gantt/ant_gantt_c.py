ee7821bf3455c183eef3dc4ddb16502a34572a1d
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-07-27 16:28:30 -1000

    Update composers to receive entire data set.

--- client/models/graph/composers/gantt/ant_gantt_c.py
+++ client/models/graph/composers/gantt/ant_gantt_c.py
@@ -1,54 +1,35 @@
 # This file contains the definition of the ant_gantt_c function.
 
-from datetime import datetime
-import time
+from ... Graph import Graph
+from .... datum.Datum import Datum
 
-def ant_gantt_c(datum, double_tuple):
+def ant_gantt_c(data, graph_meta_data):
     """
     This function processes data to be graphed as a multi-bar gantt chart.
 
     Parameters:
-        datum (Datum) : Data associated with the same timestamp.
-        double_tuple (tuple) : The graph (Graph) and its associated
-                               metadata (dict).
+        data (List) : List of Datum objs associated with the same timestamp.
+        graph_meta_data (dict) : The querry attribute, begin and end times.
 
     Returns:
         graph (Graph) : Data that will be graphed.
     """
 
-    graph = double_tuple[0]
-    graph_meta_data = double_tuple[1]
+    graph = Graph()
 
-    # replace underscore for plotly processing
-    moment = datum.timestamp.replace('_', ' ')
+    for datum in data:
+        # Add the x coordinate of the (x,y) pair
+        graph.x_values.append(datum.timestamp.replace('_', ' '))
+        # replace underscore for plotly processing
+        moment = datum.timestamp.replace('_', ' ')
 
-    # Check whether the lists with the list are empty.
-    #     i.e. ---> [ [], [], [], [], [], [], [], [] ]
-    # This is grounds for assuming that datum is the first element in the DB
-    # found to be within the time range, for this query.
-    if (not graph.gantt_values_per_antenna[0]):
-        # Append a dict to every list in gantt_values_per_antenna
-        for antenna in range(0, 8):
-            graph.gantt_values_per_antenna[antenna].append(
-                dict(
-                    Task = f"Antenna {antenna}",
-                    Start = moment,
-                    Finish = moment,
-                    Resource = f"{datum.antennas[antenna][graph_meta_data['attribute']]}"
-                )
-            )
-            # Add the color to the list of colors if it's not there, yet
-            if (datum.antennas[antenna][graph_meta_data['attribute']] not in graph.color_labels):
-                graph.color_labels.append(datum.antennas[antenna][graph_meta_data['attribute']])
-    # This isn't the first datum found within the time range.
-    else:
-        # for each of the 8 antennas
-        for antenna in range(0, 8):
-            # Update the Finish time of the Tasks
-            graph.gantt_values_per_antenna[antenna][-1]['Finish'] = moment
-            # if the color needs to change
-            if (graph.gantt_values_per_antenna[antenna][-1]['Resource'] != datum.antennas[antenna][graph_meta_data['attribute']]):
-                # add a new dict with the new value for the 'Resource' key
+        # Check whether the lists with the list are empty.
+        #     i.e. ---> [ [], [], [], [], [], [], [], [] ]
+        # This is grounds for assuming that datum is the first element in the DB
+        # found to be within the time range, for this query.
+        if (not graph.gantt_values_per_antenna[0]):
+            # Append a dict to every list in gantt_values_per_antenna
+            for antenna in range(0, 8):
                 graph.gantt_values_per_antenna[antenna].append(
                     dict(
                         Task = f"Antenna {antenna}",
@@ -60,5 +41,25 @@ def ant_gantt_c(datum, double_tuple):
                 # Add the color to the list of colors if it's not there, yet
                 if (datum.antennas[antenna][graph_meta_data['attribute']] not in graph.color_labels):
                     graph.color_labels.append(datum.antennas[antenna][graph_meta_data['attribute']])
+        # This isn't the first datum found within the time range.
+        else:
+            # for each of the 8 antennas
+            for antenna in range(0, 8):
+                # Update the Finish time of the Tasks
+                graph.gantt_values_per_antenna[antenna][-1]['Finish'] = moment
+                # if the color needs to change
+                if (graph.gantt_values_per_antenna[antenna][-1]['Resource'] != datum.antennas[antenna][graph_meta_data['attribute']]):
+                    # add a new dict with the new value for the 'Resource' key
+                    graph.gantt_values_per_antenna[antenna].append(
+                        dict(
+                            Task = f"Antenna {antenna}",
+                            Start = moment,
+                            Finish = moment,
+                            Resource = f"{datum.antennas[antenna][graph_meta_data['attribute']]}"
+                        )
+                    )
+                    # Add the color to the list of colors if it's not there, yet
+                    if (datum.antennas[antenna][graph_meta_data['attribute']] not in graph.color_labels):
+                        graph.color_labels.append(datum.antennas[antenna][graph_meta_data['attribute']])
 
     return graph
--- client/models/graph/composers/line/one_line_c.py
+++ client/models/graph/composers/line/one_line_c.py
@@ -1,20 +1,26 @@
 # This file contains the definition of the one_line_c function.
 
-def one_line_c(datum, double_tuple):
+from ... Graph import Graph
+from .... datum.Datum import Datum
+
+def one_line_c(data, graph_meta_data):
     """
     This function processes data to be graphed as a single-line line chart.
 
     Parameters:
-        datum (Datum) : Data associated with the same timestamp.
-        double_tuple (tuple) : The graph (Graph) and its associated
-                               metadata (dict).
+        data (List) : List of Datum objs associated with the same timestamp.
+        graph_meta_data (dict) : The querry attribute, begin and end times.
 
     Returns:
         graph (Graph) : Data that will be graphed.
     """
 
-    graph = double_tuple[0]
-    graph_meta_data = double_tuple[1]
-    graph.lone_y_values.append(datum[graph_meta_data['attribute']])
+    graph = Graph()
+
+    for datum in data:
+        # Add the x coordinate of the (x,y) pair
+        graph.x_values.append(datum.timestamp.replace('_', ' '))
+        # Add the y coordinate of the (x,y) pair
+        graph.lone_y_values.append(datum[graph_meta_data['attribute']])
 
     return graph
