45b508d152efd00e8064780491ced93791bc1321
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-08-02 11:19:16 -1000

    Add test for make_an_hour.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -8,7 +8,7 @@ class TestHelpers(unittest.TestCase):
     sys.path.append('.')
 
     def test_timerator(self):
-        from helpers.timerator import timerator
+        from client.helpers.timerator import timerator
         self.assertEqual(timerator("01/23/2019 12:27 PM"),"2019-01-23_12:27:00")
         self.assertEqual(timerator("01/23/2019 12:27 AM"),"2019-01-23_00:27:00")
         self.assertEqual(timerator("01/23/2019 08:27 PM"),"2019-01-23_20:27:00")
@@ -16,8 +16,10 @@ class TestHelpers(unittest.TestCase):
         self.assertEqual(timerator("01/23/2019 3:27 AM"), "2019-01-23_03:27:00")
 
     def test_make_an_hour(self):
-        from helpers.make_an_hour import make_an_hour
+        from client.helpers.make_an_hour import make_an_hour
+        # Ensure that every hour contains 60 minutes.
         self.assertEqual( len(make_an_hour(0)), 60 )
+        # Ensure that the maximum granularity of an hour is 5 seconds.
         self.assertEqual( len(make_an_hour(0)['0']), 12 )
 
 if __name__ == '__main__':
