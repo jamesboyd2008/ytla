14266abcb2bc06a6965fa8475ea037a8a3fffcd9
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-08-03 11:40:36 -1000

    Update datum_helper to not pass handicap to composers.

--- client/models/datum/datum_helper.py
+++ client/models/datum/datum_helper.py
@@ -24,34 +24,19 @@ def datum_helper(data, graph_meta_data):
     # The object that will get graphed
     graph = Graph()
 
-    end = datetime.strptime( graph_meta_data['end'], "%Y-%m-%d_%H:%M)
+    end = datetime.strptime( graph_meta_data['end'], "%Y-%m-%d_%H:%M")
     begin = datetime.strptime( graph_meta_data['begin'], "%Y-%m-%d_%H:%M")
-    count = round( ( end - begin ).total_seconds() ) / 5
+    x_val_quant = round( ( end - begin ).total_seconds() ) / 5
 
-    # To ensure that the quantity of points plotted is between ~ (12.5, 125).
-    # TODO: pick handicap more smartlier
-    if count > 1000:
-        if count > 100000000:
-            handicap = 8000000
-        elif count > 10000000:
-            handicap = 800000
-        elif count > 1000000:
-            handicap = 80000
-        elif count > 100000:
-            handicap = 8000
-        elif count > 10000:
-            handicap = 800
-    else:
-        handicap = 1
     print(f"big data.count() --> {data.count()}")
     # Choose appropriate visualization
     if (graph_meta_data['attribute'] in gantt_chart_per_antenna):
-        graph = ant_gantt_c(data, graph_meta_data, count, handicap)
+        graph = ant_gantt_c(data, graph_meta_data, x_val_quant)
     elif (graph_meta_data['attribute'] in lone_gantt_chart):
-        graph = one_gantt_c(data, graph_meta_data, count, handicap)
+        graph = one_gantt_c(data, graph_meta_data, x_val_quant)
     elif (graph_meta_data['attribute'] in line_chart_per_antenna):
-        graph = ant_line_c(data, graph_meta_data, count, handicap)
+        graph = ant_line_c(data, graph_meta_data, x_val_quant)
     else: # (graph_meta_data['attribute'] in lone_line_chart):
-        graph = one_line_c(data, graph_meta_data, count, handicap)
+        graph = one_line_c(data, graph_meta_data, x_val_quant)
 
     return graph
